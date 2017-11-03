969f638d282b1b231a1abbfae6763d22ee50983d
Nicholas Weidmann <nicholas.weidmann@hotmail.com>
2017-11-03 12:58:41 +0100

    added basic code

---
+++ test01.java
@@ -0,0 +1,13 @@
+// Welcome to the Interactive Java Tutorial.
+// Start by choosing a chapter and
+// write your code in this window.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+  }
+
+}
