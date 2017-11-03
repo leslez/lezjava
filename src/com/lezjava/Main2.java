07b76ded5f99fdb591472d93828744daebc6660e
Lea <lea.huonder95@gmail.com>
2017-11-03 18:22:28 +0100

    task 1
    not finished

--- src/com/lezjava/Main2.java
+++ src/com/lezjava/Main2.java
@@ -3,6 +3,15 @@ package com.lezjava;
 public class Main2 {
 
     public static void main(String[] args) {
-        System.out.println("test2");
+        float c = 2.0f;
+        int a = 3;
+        int b = 1;
+        int zero = 0;
+        boolean d = true;
+        char e = ' ';
+
+        String output = "H" + a + b + b + zero + e + "w" + zero + "r" + b + "d" + e + c + e + d;
+
+        System.out.println(output);
     }
 }
--- src/com/lezjava/Main3.java
+++ src/com/lezjava/Main3.java
@@ -1,8 +1,21 @@
 package com.lezjava;
 
 public class Main3 {
-
     public static void main(String[] args) {
-        System.out.println("test3");
+        String a = new String("Wow");
+        String b = "Wow";
+        String c = a;
+        String d = c;
+
+        boolean b1 = a == b;
+        boolean b2 = b.equals(b + "!");
+        boolean b3 = !c.equals(a);
+
+        if (b1 && b2 && b3) {
+                System.out.println("Success!");
+        }
+        System.out.println("b1 " + b1);
+        System.out.println("b2 " + b2);
+        System.out.println("b3 " + b3);
+        }
     }
-}
